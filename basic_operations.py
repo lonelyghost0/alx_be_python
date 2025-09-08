11026f8f0b2ac07a79f4a93c8b85075004b88230
chiamaka <122817280+lonelyghost0@users.noreply.github.com>
2025-09-08 12:40:41 +0100

    updated number

--- basic_operations.py
+++ basic_operations.py
@@ -1,5 +1,5 @@
-num1= 10
-num2 = 5 
-print(f"addition of {num1} and {num2} is {num1 +num2}")
-print(f"subtraction of {num1} and {num2} is {num1 - num2}")
-print(f"multplication of {num1} and {num2} is {num1*num2}")
+number1= 10
+number2 = 5 
+print(f"addition of {number1} and {number2} is {number1 +number2}")
+print(f"subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"multplication of {number1} and {number2} is {number1*number2}")
