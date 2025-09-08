6a6f36dd647fb76298504ba04cb756edd3567ebf
chiamaka udoye <chiamakaudoye17@gmail.com>
2025-09-08 15:26:17 +0100

    fixed the number

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
-num1 = 10
-num2 = 5
+number1 = 10
+number2 = 5
 
-print(f"Addition of {num1} and {num2} is {num1 + num2}")
-print(f"Subtraction of {num1} and {num2} is {num1 - num2}")
-print(f"Multiplication of {num1} and {num2} is {num1 * num2}")
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
