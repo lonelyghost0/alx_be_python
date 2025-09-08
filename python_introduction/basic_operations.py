73513d6659e154741859add0ec30341e49f26332
chiamaka udoye <chiamakaudoye17@gmail.com>
2025-09-08 15:23:32 +0100

    the solution

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+num1 = 10
+num2 = 5
+
+print(f"Addition of {num1} and {num2} is {num1 + num2}")
+print(f"Subtraction of {num1} and {num2} is {num1 - num2}")
+print(f"Multiplication of {num1} and {num2} is {num1 * num2}")
