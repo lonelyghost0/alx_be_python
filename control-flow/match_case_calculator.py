ceb03a1c4c9daf5279810130def257660ee56ee3
chiamaka udoye <chiamakaudoye17@gmail.com>
2025-09-15 09:15:16 +0100

    bth

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        result = num1 + num2
+        print(result)
+    case "-":
+        result = num1 - num2
+        print(result)
+    case "*":
+        result = num1 * num2
+        print(result)
+    case "/":
+        result = num1 / num2
+        print(result)
+    
