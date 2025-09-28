b97afb682d55e7c95c7acc580653b51f18c559eb
chiamaka udoye <chiamakaudoye17@gmail.com>
2025-09-28 18:47:31 +0100

    all is done

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+
+        if choice == '1':
+            add_item = input("what do you like to add to the shoping list:").lower().strip()
+            shopping_list.append(add_item)
+            # Prompt for and add an item
+            pass
+        elif choice == '2':
+            remove_item = input("what would you like to remove ").lower().strip()
+            shopping_list.remove(remove_item)
+            # Prompt for and remove an item
+            pass
+        elif choice == '3':
+            print(shopping_list)
+            # Display the shopping list
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
