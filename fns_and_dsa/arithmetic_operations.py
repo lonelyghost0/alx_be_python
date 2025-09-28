811b4979a2482773d2d91e62090d82a85ec0834e
chiamaka udoye <chiamakaudoye17@gmail.com>
2025-09-28 18:13:06 +0100

    first task

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,23 @@
+def perform_operation (num1,num2,operation):
+
+  match operation:
+
+    case "add" :
+      return num1 + num2 
+    
+    case "subtract":
+      return num1 - num2
+    
+    case "multiply":
+      return num1*num2
+    
+    case "divide":
+      if num2 != 0:
+        result = num1 / num2
+        print(f"The result is {result}")
+      else:
+        print("cnat divide by 0")
+
+
+        
+
