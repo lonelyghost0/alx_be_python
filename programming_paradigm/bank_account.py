4dabb96e01aa716bcd2ec085dc32480e5bb5b33c
chiamaka <122817280+lonelyghost0@users.noreply.github.com>
2025-10-06 08:19:01 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,23 +1,20 @@
 class BankAccount:
 
-    def  __init__(self, account_balance):
-        self.account_balance = account_balance
-        account_balance = 0
+    def  __init__(self, inital_balance = 0):
+        self.account_balance = inital_balance
+        
     
     def deposit (self, amount):
-        amount = int(input("how much would you like to deposit : "))
         self.account_balance += amount
 
     def  withdraw(self, amount):
-        amount = int(input("how much would you like to withdraw :"))
-        while True :
-            if self.account_balance < amount:
-                print (" you do nit have the money to withdraq the amount ")
-                False
-            else :
-                self.account_balance -= amount
-                print (f"ypu have withdrawn {amount}, the balaance is now {self.account_balance}")
-                break
+        if self.account_balance < amount:
+             print (" you do nit have the money to withdraq the amount ")
+             return False
+        else :
+             self.account_balance -= amount
+            print (f"ypu have withdrawn {amount}, the balaance is now {self.account_balance}")
+            return True
 
     def display_balance(self):
         print(self.account_balance)
